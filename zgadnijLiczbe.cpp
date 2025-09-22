173683ee9703bf2d6b5b59919f6d4b1f99f4f889
Bubliks <149240768+Bubliksiur@users.noreply.github.com>
2025-09-22 21:35:04 +0200

    Add files via upload

---
+++ zgadnijLiczbe.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void ZgadnijLiczbe()
+{
+    cout << "Witaj w prostym programie w ktorym musisz zgadnac liczbe od 1 do 100" << endl;
+    while (true)
+    {
+        int liczba = rand() % 100 + 1;
+        int wyborUzytkownika;
+        cout << "Wpisz liczbe od 1 do 100: ";
+        cin >> wyborUzytkownika;
+
+        if (wyborUzytkownika < liczba)
+        {
+            cout << "Twoja liczba jest za mala!" << endl;
+            continue;
+        }
+        else if (wyborUzytkownika > liczba)
+        {
+            cout << "Twoja liczba jest za duza!" << endl;
+            continue;
+        }
+        else
+        {
+            cout << "Udalo ci się zgadnac liczbe" << endl;
+            break;
+        }
+    }
+}
+int main()
+{
+    ZgadnijLiczbe();
+    return 0;
+}
